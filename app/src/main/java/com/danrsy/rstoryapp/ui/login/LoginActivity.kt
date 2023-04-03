d859ed558704ceca259e9e0ec174a1470f68a22f
RadRasyad <rafidanyr21@gmail.com>
2023-04-03 15:12:19 +0700

    Initial commit

---
+++ app/src/main/java/com/danrsy/rstoryapp/ui/login/LoginActivity.kt
@@ -0,0 +1,16 @@
+package com.danrsy.rstoryapp.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.danrsy.rstoryapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
